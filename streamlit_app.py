a36965b352a15571959ab77d0bc0ed8681658c7a
suzukiniko <168629217+suzukiniko@users.noreply.github.com>
2024-05-02 09:18:25 +0000

    omikuzi

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,8 @@ st.title("おみくじアプリ")
 
 if  st.button("おみくじを引く"):
     results=["大吉","吉","中吉","小吉","凶","大凶"]
-    result=random.choice(results)   
+    result=random.choice(results)
+    st.write(f"結果:{result}")
 
 #結果に応じたコメントやアドバイス
     comments={
