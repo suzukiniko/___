d96bbcbf31a18042630a7b18d84ee17e45000260
suzukiniko <168629217+suzukiniko@users.noreply.github.com>
2024-05-08 07:54:00 +0000

    ｖ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,23 +1,11 @@
 # Streamlitライブラリをインポート
 import streamlit as st
-import random
 
 # タイトルを設定
-st.title("おみくじアプリ")
+    st.title("BMI診断")
 
-#結果に応じたコメントやアドバイス
-comments={
-    "大吉":"おめでとう！素晴らしい一日になりそうです！！",
-    "吉":"一日ラッキーな日になるでしょう。",
-    "中吉":"良いことがおこるかも？",
-    "小吉":"何もない一日になるといいですが...",
-    "凶":"周りに気をつけましょう。",
-    "大凶":"生きて帰れるといいですね^^",
-    }
-
-if  st.button("おみくじを引く"):
-    results=["大吉","吉","中吉","小吉","凶","大凶"]
-    result=random.choice(results)
-    st.write(f"結果:{result}"+
-    comments)
-
+    st.write("身長と体重を入力してください。")
+    weight=st.text_input("体重")
+    tall=st.text_imput("身長")
+    bmi= weight / ( tall * tall )
+    st.write("あなたのBMIは"+str(bmi)+です。)
