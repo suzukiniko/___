76967fed4ca38261a07eea8504b196e2508f51f5
suzukiniko <168629217+suzukiniko@users.noreply.github.com>
2024-05-08 08:26:06 +0000

    hgfd

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,5 @@ st.write("身長と体重を入力してください。")
 weight=st.number_input("体重")
 tall=st.number_input("身長")
 if st.button("BMIを計測"):
-bmi= weight /  tall ** 
+bmi=weight/tall**
 st.write("あなたのBMIは"+str(bmi)+"です。")
