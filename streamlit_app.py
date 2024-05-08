4cdf47fb2f1a545dbf912822f386d1c97b74ea23
suzukiniko <168629217+suzukiniko@users.noreply.github.com>
2024-05-08 07:54:23 +0000

    s

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ import streamlit as st
     weight=st.text_input("体重")
     tall=st.text_imput("身長")
     bmi= weight / ( tall * tall )
-    st.write("あなたのBMIは"+str(bmi)+です。)
+    st.write("あなたのBMIは"+str(bmi)+"です。")
