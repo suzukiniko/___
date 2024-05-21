d19f078ad86aab7ff08227e81670c92fb7732616
suzukiniko <168629217+suzukiniko@users.noreply.github.com>
2024-05-21 08:36:10 +0000

    vdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,12 +2,4 @@
 import streamlit as st
 
 # タイトルを設定
-st.title("BMI診断")
-
-st.write("身長と体重を入力してください。")
-weight=st.number_input("体重",min_value=1.0)
-tall=st.number_input("身長",min_value=1.0)
-if st.button("BMIを計測"):
-    bmi=weight/tall**2
-    rounded_bmi=round(bmi, 2)
-st.write("あなたのBMIは"+str(rounded_bmi)+"です。")
+st.title("歴史単語ガチャ")
