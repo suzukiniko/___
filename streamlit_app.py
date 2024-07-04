50cade3275f11f4c1299e04d880dc4892d1c0706
suzukiniko <168629217+suzukiniko@users.noreply.github.com>
2024-07-04 08:34:27 +0000

    ないｌ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,15 @@
 import streamlit as st
 
 # タイトルを設定
-st.title("歴史単語ガチャ")
+import plotly.express as px
 
-if st.button("始める"):
-    st.write("桜田門外の変")
+# データフレームを作成
+df = px.data.gapminder().query("country=='Japan'")
+
+# 地図を描画
+fig = px.scatter_geo(df, locations="iso_alpha",
+                     size="pop", # ポイントのサイズを人口に応じて変える
+                     )
+
+# 地図を表示
+fig.show()
