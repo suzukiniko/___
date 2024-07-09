ddd9e185bf6c8405a2b94bdb26de78e71ec95e80
suzukiniko <168629217+suzukiniko@users.noreply.github.com>
2024-07-09 09:08:38 +0000

    ｃｓｄ

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,20 +2,7 @@
 import streamlit as st
 
 # タイトルを設定
-import tempfile
-import webbrowser
-from PIL import Image, ImageDraw
+st.title("歴史人物フラッシュゲーム")
 
-# 仮の画像を作成する（例として赤い四角形を描画）
-image = Image.new('RGB', (200, 200), color='blue')
-draw = ImageDraw.Draw(image)
-draw.rectangle([50, 50, 150, 150], fill='red')
-
-# 一時ファイルに保存する
-temp_image_path = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
-image.save(temp_image_path.name)
-temp_image_path.close()
-
-# 一時ファイルをブラウザで開く
-webbrowser.open('file://' + temp_image_path.name)
+st.write("これから様々な歴史上の偉人が表示されるので、できるだけ早く回答してください！")
 
