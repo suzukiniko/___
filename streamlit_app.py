30317fae7aa4726980e26415a2f3575217867b8a
suzukiniko <168629217+suzukiniko@users.noreply.github.com>
2024-07-09 08:32:58 +0000

    ｄｘｊｎ

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import webbrowser
 from PIL import Image, ImageDraw
 
 # 仮の画像を作成する（例として赤い四角形を描画）
-image = Image.new('RGB', (200, 200), color='white')
+image = Image.new('RGB', (200, 200), color='blue')
 draw = ImageDraw.Draw(image)
 draw.rectangle([50, 50, 150, 150], fill='red')
 
