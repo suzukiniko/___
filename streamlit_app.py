53bf8bb94a407bbade709b5c9bf7ac1eaa86f05d
suzukiniko <168629217+suzukiniko@users.noreply.github.com>
2024-08-07 00:56:29 +0000

    fyjfcxrthcryjctyj

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,18 @@
 # Streamlitライブラリをインポート
 import streamlit as st
-
+import re
 # タイトルを設定
 st.title("歴史人物フラッシュゲーム")
 
 st.write("これから様々な歴史上の偉人が表示されるので、できるだけ早く回答してください！")
-
+#背景色の設定
+st.write(
+    f"""
+    <style>
+        .stApp{{
+            background-color: #abced8;
+        }}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
